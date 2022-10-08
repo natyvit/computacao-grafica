453e7ca9ba1ef12095ff7340f0c278b5dfea22b1
flaviotech <flavio.nogueira.profissional@gmail.com>
2022-10-08 20:46:52 -0300

    Retirando coisas desnecessárias

--- trabalho_unidade2/flavio/pirade_base_quadrangular/scene.js
+++ trabalho_unidade2/flavio/pirade_base_quadrangular/scene.js
@@ -10,21 +10,9 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({
-    color: 0xcd007f,
-    wireframe: true,
-});
-
 camera.position.z = 5;
 
 const quad = new THREE.BufferGeometry();
-const vertices = new Float32Array([
-    -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0,
-
-    1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0,
-]);
-
 const piramide = new THREE.BufferGeometry();
 
 const base = [
@@ -49,12 +37,12 @@ const face4 = [
     0.0, 1.0, 0.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0,
 ]
 
-const vert = new Float32Array(
+const vertices = new Float32Array(
     Array.prototype.concat(base, face1, face2, face3, face4)
 );
 
-piramide.setAttribute("position", new THREE.BufferAttribute(vert, 3));
-const mat = new THREE.MeshBasicMaterial({ color: 0xcd007f, wireframe: true });
+piramide.setAttribute("position", new THREE.BufferAttribute(vertices, 3));
+const mat = new THREE.MeshBasicMaterial({ color: 0x00ffdd, wireframe: true });
 const mesh = new THREE.Mesh(piramide, mat);
 scene.add(mesh);
 
