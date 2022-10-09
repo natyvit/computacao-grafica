da8b00065fa0b5fb3e2ff41713d00fe8b53e8b83
flaviotech <flavio.nogueira.profissional@gmail.com>
2022-10-08 22:46:15 -0300

    Mapeando coordenas UVs da forma correta

--- trabalho_unidade2/flavio/pirade_base_quadrangular/scene_pyramid_example.js
+++ trabalho_unidade2/flavio/pirade_base_quadrangular/scene_pyramid_example.js
@@ -1,6 +1,6 @@
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
-    150,
+    75,
     window.innerWidth / window.innerHeight,
     0.1,
     1000
@@ -16,37 +16,37 @@ const quad = new THREE.BufferGeometry();
 const piramide = new THREE.BufferGeometry();
 
 const base = [
-    -3.0, 0.0, 3.0,
-    3.0, 0.0, 3.0,
-    3.0, 0.0, -3.0,
+    -1.0, -1.0, 1.0,
+    1.0, -1.0, 1.0,
+    1.0, -1.0, -1.0,
 
-    3.0, 0.0, -3.0,
-    -3.0, 0.0, -3.0,
-    -3.0, 0.0, 3.0,
+    1.0, -1.0, -1.0,
+    -1.0, -1.0, -1.0,
+    -1.0, -1.0, 1.0,
 ]
 
 const face1 = [
-    -3.0, 0.0, 3.0,
-    3.0, 0.0, 3.0,
-    0.0, 5.0, 0.0,
+    -1.0, -1.0, 1.0,
+    1.0, -1.0, 1.0,
+    0.0, 1.0, 0.0,
 ]
 
 const face2 = [
-    3.0, 0.0, 3.0,
-    3.0, 0.0, -3.0,
-    0.0, 5.0, 0.0,
+    1.0, -1.0, 1.0,
+    1.0, -1.0, -1.0,
+    0.0, 1.0, 0.0,
 ]
 
 const face3 = [
-    3.0, 0.0, -3.0,
-    -3.0, 0.0, -3.0,
-    0.0, 5.0, 0.0,
+    1.0, -1.0, -1.0,
+    -1.0, -1.0, -1.0,
+    0.0, 1.0, 0.0,
 ]
 
 const face4 = [
-    -3.0, 0.0, -3.0,
-    -3.0, 0.0, 3.0,
-    0.0, 5.0, 0.0,
+    -1.0, -1.0, -1.0,
+    -1.0, -1.0, 1.0,
+    0.0, 1.0, 0.0,
 ]
 
 const vertices = new Float32Array(
@@ -54,29 +54,29 @@ const vertices = new Float32Array(
 );
 
 const uvs = new Float32Array([
-    0.0, 0.0,
-    1.0, 0.0,
-    1.0, 1.0,
+	0.0, 0.0,
+	1.0, 0.0,
+	1.0, 1.0,
 
     1.0, 1.0,
-    0.0, 1.0,
-    0.0, 0.0,
+	0.0, 1.0,
+	0.0, 0.0,
 
-    0.0, 0.0,
-    1.0, 0.0,
-    0.5, 1.0,
+	0.0, 0.0,
+	1.0, 0.0,
+	0.5, 1.0,
 
-    0.0, 0.0,
-    1.0, 0.0,
-    0.5, 1.0,
+	0.0, 0.0,
+	1.0, 0.0,
+	0.5, 1.0,
 
-    0.0, 0.0,
-    1.0, 0.0,
-    0.5, 1.0,
+	0.0, 0.0,
+	1.0, 0.0,
+	0.5, 1.0,
 
-    0.0, 0.0,
-    1.0, 0.0,
-    0.5, 1.0,
+	0.0, 0.0,
+	1.0, 0.0,
+	0.5, 1.0,
 ]);
 
 const texture = new THREE.TextureLoader().load('./static/pyramid_texture.jpg');
@@ -88,14 +88,12 @@ piramide.setAttribute('uv', new THREE.BufferAttribute(uvs, 2));
 // const mat = new THREE.MeshBasicMaterial({ color: 0x00ffdd, wireframe: true });
 const mat = new THREE.MeshBasicMaterial({ map: texture, side: THREE.DoubleSide });
 const mesh = new THREE.Mesh(piramide, mat);
-mesh.rotation.x = 0.5;
-
 scene.add(mesh);
 
 
 function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
-    mesh.rotation.y += 0.01;
+    mesh.rotation.y += 0.02;
 }
 animate();
