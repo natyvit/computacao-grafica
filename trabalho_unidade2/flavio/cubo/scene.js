d6f53a6b79ad62f2ae51f14f50b4ab2af1199907
flaviotech <flavio.nogueira.profissional@gmail.com>
2022-10-09 12:15:57 -0300

    Exemplo de cubo

--- trabalho_unidade2/flavio/cubo/scene.js
+++ trabalho_unidade2/flavio/cubo/scene.js
@@ -1,6 +1,6 @@
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
-    170,
+    150,
     window.innerWidth / window.innerHeight,
     0.1,
     1000
---
+++ trabalho_unidade2/flavio/cubo/scene_cube_example.js
@@ -0,0 +1,149 @@
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+camera.position.z = 5;
+
+const quad = new THREE.BufferGeometry();
+const cube = new THREE.BufferGeometry();
+
+const base = [
+    -1.0, -1.0, 1.0,
+    1.0, -1.0, 1.0,
+    1.0, -1.0, -1.0,
+
+    1.0, -1.0, -1.0,
+    -1.0, -1.0, -1.0,
+    -1.0, -1.0, 1.0,
+]
+
+const face1 = [
+    -1.0, -1.0, 1.0,
+    1.0, -1.0, 1.0,
+    1.0, 1.0, 1.0,
+
+    1.0, 1.0, 1.0,
+    -1.0, 1.0, 1.0,
+    -1.0, -1.0, 1.0,
+]
+
+const face2 = [
+    1.0, -1.0, 1.0,
+    1.0, -1.0, -1.0,
+    1.0, 1.0, -1.0,
+
+    1.0, 1.0, -1.0,
+    1.0, 1.0, 1.0,
+    1.0, -1.0, 1.0,
+]
+
+const face3 = [
+    1.0, -1.0, -1.0,
+    -1.0, -1.0, -1.0,
+    -1.0, 1.0, -1.0,
+
+    -1.0, 1.0, -1.0,
+    1.0, 1.0, -1.0,
+    1.0, -1.0, -1.0,
+]
+
+const face4 = [
+    -1.0, -1.0, -1.0,
+    -1.0, -1.0, 1.0,
+    -1.0, 1.0, 1.0,
+
+    -1.0, 1.0, 1.0,
+    -1.0, 1.0, -1.0,
+    -1.0, -1.0, -1.0,
+]
+
+const topo = [
+    -1.0, 1.0, 1.0,
+    1.0, 1.0, 1.0,
+    1.0, 1.0, -1.0,
+
+    1.0, 1.0, -1.0,
+    -1.0, 1.0, -1.0,
+    -1.0, 1.0, 1.0,
+]
+
+const vertices = new Float32Array(
+    Array.prototype.concat(base, face1, face2, face3, face4, topo)
+);
+
+const uvs = new Float32Array([
+	0.0, 0.0,
+	1.0, 0.0,
+	1.0, 1.0,
+
+    1.0, 1.0,
+	0.0, 1.0,
+	0.0, 0.0,
+
+	0.0, 0.0,
+	1.0, 0.0,
+	1.0, 1.0,
+
+    1.0, 1.0,
+	0.0, 1.0,
+	0.0, 0.0,
+
+	0.0, 0.0,
+	1.0, 0.0,
+	1.0, 1.0,
+
+    1.0, 1.0,
+	0.0, 1.0,
+	0.0, 0.0,
+
+	0.0, 0.0,
+	1.0, 0.0,
+	1.0, 1.0,
+
+    1.0, 1.0,
+	0.0, 1.0,
+	0.0, 0.0,
+
+	0.0, 0.0,
+	1.0, 0.0,
+	1.0, 1.0,
+
+    1.0, 1.0,
+	0.0, 1.0,
+	0.0, 0.0,
+
+	0.0, 0.0,
+	1.0, 0.0,
+	1.0, 1.0,
+
+    1.0, 1.0,
+	0.0, 1.0,
+	0.0, 0.0,
+]);
+
+const texture = new THREE.TextureLoader().load('./static/cube_texture.jpg');
+
+
+cube.setAttribute("position", new THREE.BufferAttribute(vertices, 3));
+cube.setAttribute('uv', new THREE.BufferAttribute(uvs, 2));
+
+// const mat = new THREE.MeshBasicMaterial({ color: 0x00ffdd, wireframe: true });
+const mat = new THREE.MeshBasicMaterial({ map: texture, side: THREE.DoubleSide });
+const mesh = new THREE.Mesh(cube, mat);
+scene.add(mesh);
+
+
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+    mesh.rotation.y += 0.01;
+}
+animate();
